2b789dab0049ea41841b42683f05c5a0eaf6e749
Forest <forestlaw5566@gmail.com>
2025-12-02 05:02:45 -0300

    Fix STATIC_ROOT para collectstatic en producción

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
+set -o errexit
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn historias_web.wsgi:application --bind 0.0.0.0:10000
 
-gunicorn plataforma.wsgi:application --bind 0.0.0.0:$PORT
